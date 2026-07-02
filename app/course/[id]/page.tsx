3fb0d6d306a6cee03ce7a42c4067b28153edc0c7
kapspramoda <kapspramoda@mail.com>
2026-07-02 08:00:36 +0530

    Add student security-3

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 type PageProps = {
