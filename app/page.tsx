c8a7125b0bfa18883a34bb3329c24d166c16c874
kapspramoda <kapspramoda@mail.com>
2026-07-20 06:10:52 +0530

    remove fb icon

--- app/page.tsx
+++ app/page.tsx
@@ -516,7 +516,7 @@ export default function HomePage() {
               
               <div className="mt-6 flex space-x-5">
                 <a href="https://www.facebook.com/share/1D4uc4UiGn/?mibextid=wwXIfr" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-blue-500 transition-colors" title="Facebook">
-                  {/* <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" /></svg> */}
+                 
                 </a>
                 <a href="https://youtube.com/@20minuteslk?si=YhilxLi7_6FPuk9X" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-red-500 transition-colors" title="YouTube">
                   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd" /></svg>
